9195e88b2af7d24dac42b859b1d48e9a16d4b18e
Giulia Creps <giulia.creps2@gmail.com>
2025-12-04 16:40:56 +0100

    Adds exercises

--- D2.js
+++ D2.js
@@ -4,11 +4,23 @@
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+let numbMajor = 10;
+let numbMinor = 5;
+
+if (numbMajor > numbMinor) {
+  console.log(numbMajor);
+}
+
 /* ESERCIZIO 2
   Scrivi un algoritmo che mostri "not equal" in console se un numero intero fornito è diverso da 5.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+let numbNotEqual = 7;
+if (numbNotEqual !== 5) {
+  console.log("not equal");
+}
 
 /* ESERCIZIO 3
   Scrivi un algoritmo che mostri "divisibile per 5" in console se un numero fornito è perfettamente divisibile per 5 (suggerimento: usa l'operatore modulo)
@@ -16,11 +28,23 @@
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+let numbDivisibile = 15;
+if (numbDivisibile % 5 === 0) {
+  console.log("divisibile per 5");
+}
+
 /* ESERCIZIO 4
   Scrivi un algoritmo per verificare che, dati due numeri interi, il valore di uno di essi sia 8 oppure se la loro addizione/sottrazione sia uguale a 8.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+let numbInteger1 = 5;
+let numbInteger2 = 3;
+
+if (numbInteger1 === 8 || numbInteger2 === 8 || numbInteger1 + numbInteger2 === 8 || numbInteger1 - numbInteger2 === 8) {
+  console.log("Una delle condizioni è vera");
+}
 
 /* ESERCIZIO 5
   Stai lavorando su un sito di e-commerce. Stai salvando il saldo totale del carrello dell'utente in una variabile "totalShoppingCart".
@@ -30,12 +54,40 @@
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+let totalShoppingCart = 60;
+let shippingCost = 10;
+
+if (totalShoppingCart > 50) {
+  console.log("La spedizione è gratuita. Il totale da pagare è: " + totalShoppingCart);
+} else if (totalShoppingCart < 50) {
+  shippingCost = 10;
+  const totalWithShipping = totalShoppingCart + shippingCost;
+  console.log("Il totale da pagare è: " + totalWithShipping);
+}
+
 /* ESERCIZIO 6
   Stai lavorando su un sito di e-commerce. Oggi è il Black Friday e viene applicato il 20% su ogni prodotto.
   Modifica la risposta precedente includendo questa nuova promozione nell'algoritmo, determinando come prima se le spedizioni sono gratuite oppure no e e calcolando il totale.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+let totalShoppingBlackFriday = 0.2;
+let totalAmount = 100;
+let shippingCostBlackFriday = 10;
+
+if ((totalShoppingBlackFriday = 0.2)) {
+  const costMerchandisingBlackFriday = totalAmount - totalShoppingBlackFriday;
+  console.log("Il totale scontato è: " + costMerchandisingBlackFriday);
+}
+if (totalShoppingCart > 50) {
+  const costMerchandisingBlackFriday = totalAmount - shippingCostBlackFriday;
+  console.log("La spedizione è gratuita. Il totale da pagare è: " + costMerchandisingBlackFriday);
+} else if (totalShoppingCart < 50) {
+  shippingCostBlackFriday = 10;
+  const totalWithShippingBlackFriday = totalAmount + shippingCostBlackFriday;
+  console.log("Il totale da pagare è: " + totalWithShippingBlackFriday);
+}
 
 /* ESERCIZIO 7
   Crea tre variabili, e assegna un valore numerico a ciascuna di esse.
@@ -45,11 +97,37 @@
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+let x = 15;
+let y = 7;
+let z = 5;
+
+if (x > y && x > z) {
+  if (y > z) {
+    console.log(x, y, z);
+  } else {
+    console.log(x, z, y);
+  }
+}
+
 /* ESERCIZIO 8
   Crea un algoritmo per verificare che un valore fornito sia un numero oppure no (suggerimento: cerca su un motore di ricerca "typeof").
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+let value = 25;
+if (typeof value === "number") {
+  console.log("Il valore è un numero");
+} else {
+  console.log("Il valore non è un numero");
+}
+
+//alternativo
+let value2 = "25";
+console.log(typeof value2 === "number" ? "Il valore è un numero" : "Il valore non è un numero");
+
+//alternativo 2
+console.log(typeof value2);
 
 /* ESERCIZIO 9
   Crea un algoritmo per controllare se un numero fornito sia pari o dispari (suggerimento: cerca l'operatore modulo su un motore di ricerca)
@@ -57,17 +135,24 @@
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
-/* ESERCIZIO 10
-  Modifica la logica del seguente algoritmo in modo che mostri in console il messaggio corretto in ogni circostanza.
-  let val = 7
-  if (val < 10) {
-      console.log("Meno di 10");
-    } else if (val < 5) {
-      console.log("Meno di 5");
-    } else {
-      console.log("Uguale a 10 o maggiore");
-    }
-*/
+let numberToCheck = 8;
+if (numberToCheck % 2 === 0) {
+  console.log("Il numero è pari");
+} else {
+  console.log("Il numero è dispari");
+}
+
+// ESERCIZIO 10
+//Modifica la logica del seguente algoritmo in modo che mostri in console il messaggio corretto in ogni circostanza.
+let val = 7;
+if (val < 10) {
+  console.log("Meno di 10");
+}
+if (val < 5) {
+  console.log("Meno di 5");
+} else if (val >= 10) {
+  console.log("Uguale a 10 o maggiore");
+}
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
@@ -76,10 +161,10 @@
 */
 
 const me = {
-  name: 'John',
-  lastName: 'Doe',
-  skills: ['javascript', 'html', 'css'],
-}
+  name: "John",
+  lastName: "Doe",
+  skills: ["javascript", "html", "css"],
+};
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
